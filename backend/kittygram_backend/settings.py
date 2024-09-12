b319c326f201ba4bf718ace9009bb25ca2b54971
Alexey Lysov <novice83@mail.ru>
2024-09-12 16:15:31 +0300

    Version 1.0

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,13 +2,21 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+DEFAULT_DB_PORT = 5432
 
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
+SERVER_IP = os.getenv('SERVER_IP')
+SERVER_DOMAIN = os.getenv('SERVER_DOMAIN')
 
-ALLOWED_HOSTS = []
+DEBUG = bool(os.getenv('DEBUG'))
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', SERVER_IP, SERVER_DOMAIN]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -56,8 +64,12 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', DEFAULT_DB_PORT)
     }
 }
 
@@ -92,6 +104,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
